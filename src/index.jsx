57e861d268a68aa358f6deded9600812424d3f81
Vinícius Monaco <viniciussitomonaco@gmai.com>
2023-10-14 16:58:12 -0300

    adicionando carrossel de promo

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Introduction from "./components/Introduction";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import Introduction from "./components/initialView/Introduction";
+import Header from "./components/header/Header";
+import CarouselPromoArea from "./components/carouselPromo/CarouselPromoArea"
 import './index.css'
 
 const root = document.querySelector('#root');
@@ -11,6 +11,6 @@ ReactDOM.render(
     <React.StrictMode>
         <Header />
         <Introduction />
-        <Main />
+        <CarouselPromoArea />
     </React.StrictMode>
 ,root);
